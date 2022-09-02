dfae0b086292d40d80f75e7f57fe3daf4c1e60c2
TimAlber <t.midaidu@gmail.com>
2022-09-02 11:51:50 +0200

    sort of done, making pritty now

--- lib/favorite_page.dart
+++ lib/favorite_page.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 
+class FactHolder {
+  FactHolder({
+    required this.id,
+    required this.fact,
+
+  });
+  String id;
+  String fact;
+}
+
 class FavoritePage extends StatefulWidget {
   const FavoritePage({Key? key}) : super(key: key);
 
@@ -9,12 +19,11 @@ class FavoritePage extends StatefulWidget {
 }
 
 class _FavoritePageState extends State<FavoritePage> {
-  var favoriteFacts = <String>[];
+  var favoriteFacts = <FactHolder>[];
 
   @override
   void initState() {
     _getFavFacts().then((value) => {
-      print('list: ' + value.toString()),
       setState(() {
         favoriteFacts = value;
       }),
@@ -24,10 +33,17 @@ class _FavoritePageState extends State<FavoritePage> {
 
   @override
   Widget build(BuildContext context) {
+    favoriteFacts.sort((a, b) => a.fact.compareTo(b.fact));
     return ListView.separated(
       itemBuilder: (context, index) {
         return ListTile(
-          title: Text(favoriteFacts[index]),
+          title: Text(favoriteFacts[index].fact),
+          trailing: IconButton(
+            icon: const Icon(Icons.delete_forever),
+            onPressed: () => {
+              _deleteFav(favoriteFacts[index].id),
+            },
+          ),
         );
       },
       separatorBuilder: (context, index) {
@@ -39,12 +55,25 @@ class _FavoritePageState extends State<FavoritePage> {
     );
   }
 
-  Future<List<String>> _getFavFacts() async {
-    final ouput = <String>[];
+  Future<void> _deleteFav(String key) async {
+    var box = await Hive.openBox('factBox');
+    box.delete(key);
+    box.close();
+
+    setState(() {
+      favoriteFacts.removeWhere((element) => element.id == key);
+    });
+  }
+
+  Future<List<FactHolder>> _getFavFacts() async {
+    final ouput = <FactHolder>[];
     var box = await Hive.openBox('factBox');
     for (final key in box.keys) {
       final fact = box.get(key).toString();
-      ouput.add(fact);
+      ouput.add(FactHolder(
+        id: key,
+        fact: fact,
+      ));
     }
     box.close();
     return ouput;
