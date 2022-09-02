0d4267de3f1c195cae3030fe1bab369ae849a5e5
TimAlber <t.midaidu@gmail.com>
2022-09-02 12:42:11 +0200

    make work on web

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,14 @@ import 'package:useless_facts/home_page.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:hive/hive.dart';
 
+import 'package:flutter/foundation.dart' show kIsWeb;
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  var dir = await getApplicationDocumentsDirectory();
-  Hive.init(dir.path);
+  if(!kIsWeb){
+    var dir = await getApplicationDocumentsDirectory();
+    Hive.init(dir.path);
+  }
   runApp(const MyApp());
 }
 
